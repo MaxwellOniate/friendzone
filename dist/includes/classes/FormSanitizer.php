dde398dff5c5157f9a08306ad9bdac06ba6fb55b
maxwelloniate <maxwelloniate@gmail.com>
2020-02-14 16:33:47 -0500

    Form sanitizers

---
+++ dist/includes/classes/FormSanitizer.php
@@ -0,0 +1,23 @@
+<?php
+
+class FormSanitizer
+{
+  public static function sanitizeFormString($input)
+  {
+    $input = strip_tags($input);
+    $input  = str_replace(' ', '', $input);
+    $input = ucfirst(strtolower($input));
+    return $input;
+  }
+  public static function sanitizeFromEmail($input)
+  {
+    $input = strip_tags($input);
+    $input = str_replace(" ", "", $input);
+    return $input;
+  }
+  public static function sanitizeFormPassword($input)
+  {
+    $input = strip_tags($input);
+    return $input;
+  }
+}
