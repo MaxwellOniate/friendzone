5cf15ec6f330a719f18b3cb68c7a09382a472a99
maxwelloniate <maxwelloniate@gmail.com>
2020-02-14 18:32:34 -0500

    Form input validation

--- dist/includes/classes/Account.php
+++ dist/includes/classes/Account.php
@@ -9,4 +9,66 @@ class Account
   {
     $this->con = $con;
   }
+
+  public function validateFirstName($fn)
+  {
+    if (strlen($fn) < 2 || strlen($fn) > 25) {
+      array_push($this->errorArray, Constants::$firstNameCharacters);
+      return;
+    }
+  }
+
+  public function validateLastName($ln)
+  {
+    if (strlen($ln) < 2 || strlen($ln) > 25) {
+      array_push($this->errorArray, Constants::$lastNameCharacters);
+      return;
+    }
+  }
+
+  public function validateUsername($un)
+  {
+    if (strlen($un) < 2 || strlen($un) > 25) {
+      array_push($this->errorArray, Constants::$usernameCharacters);
+      return;
+    }
+
+    $query = $this->con->prepeare("SELECT * FROM users WHERE username = :un");
+    $query->execute([':un' => $un]);
+
+    if ($query->rowCount() != 0) {
+      array_push($this->errorArray, Constants::$usernameTaken);
+    }
+  }
+
+  public function validateEmails($em, $em2)
+  {
+    if ($em != $em2) {
+      array_push($this->errorArray, Constants::$emailsDontMatch);
+      return;
+    }
+
+    if (!filter_var($em, FILTER_VALIDATE_EMAIL)) {
+      array_push($this->errorArray, Constants::$emailInvalid);
+    }
+
+    $query = $this->con->prepare("SELECT * FROM users WHERE email = :em");
+    $query->execute([':em' => $em]);
+
+    if ($query->rowCount != 0) {
+      array_push($this->errorArray, Constants::$emailTaken);
+    }
+  }
+
+  public function validatePasswords($pw, $pw2)
+  {
+    if ($pw != $pw2) {
+      array_push($this->errorArray, Constants::$passwordsDontMatch);
+      return;
+    }
+
+    if (strlen($pw) < 8 || strlen($pw) > 99) {
+      array_push($this->errorArray, Constants::$passwordLength);
+    }
+  }
 }
