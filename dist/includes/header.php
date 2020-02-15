3ccf1a5f89b3462c98ad3e0d3b28aca5d0bbf0fb
maxwelloniate <maxwelloniate@gmail.com>
2020-02-15 12:43:00 -0500

    Font awesome + favicon

--- dist/includes/header.php
+++ dist/includes/header.php
@@ -5,9 +5,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Friendzone</title>
+  <script src="https://kit.fontawesome.com/52d1564875.js" crossorigin="anonymous"></script>
   <script src="assets/js/jquery.min.js"></script>
   <script src="assets/js/popper.min.js"></script>
   <script src="assets/js/bootstrap.min.js"></script>
+  <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="assets/css/bootstrap.css">
   <link rel="stylesheet" href="assets/css/main.css">
 </head>
