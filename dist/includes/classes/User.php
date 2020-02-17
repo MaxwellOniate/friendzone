2c93ecd2aef8c70b4bf37fbd6ade538ecbc7945f
maxwelloniate <maxwelloniate@gmail.com>
2020-02-17 13:07:55 -0500

    User class and getter functions

---
+++ dist/includes/classes/User.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class User
+{
+  private $con, $email, $sqlData;
+
+  public function __construct($con, $email)
+  {
+    $this->con = $con;
+    $this->email = $email;
+
+    $query = $con->prepare("SELECT * FROM users WHERE email = :em");
+    $query->execute([':em' => $email]);
+
+    $this->sqlData = $query->fetch(PDO::FETCH_ASSOC);
+  }
+  public function getFirstName()
+  {
+    return $this->sqlData['firstName'];
+  }
+  public function getLastName()
+  {
+    return $this->sqlData['lastName'];
+  }
+  public function getEmail()
+  {
+    return $this->email;
+  }
+  public function getProfilePic()
+  {
+    return $this->sqlData['profilePic'];
+  }
+}
