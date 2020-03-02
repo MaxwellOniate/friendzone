de2f23766c515fabc528cf0cc62381792b0ce4fb
maxwelloniate <maxwelloniate@gmail.com>
2020-03-02 17:46:45 -0500

    Added public function getFriendCount

--- dist/includes/classes/User.php
+++ dist/includes/classes/User.php
@@ -47,6 +47,10 @@ class User
   {
     return $this->sqlData['num_likes'];
   }
+  public function getFriendCount()
+  {
+    return (substr_count($this->sqlData['friend_array'], ",")) - 1;
+  }
   public function isClosed()
   {
     if ($this->sqlData['user_closed'] == 'yes') {
