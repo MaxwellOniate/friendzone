e7bce885082298d91b9385ab46c1d1c7b6140d99
maxwelloniate <maxwelloniate@gmail.com>
2020-03-29 01:26:28 -0400

    config

--- dist/includes/config.php
+++ dist/includes/config.php
@@ -5,10 +5,9 @@ session_start();
 
 date_default_timezone_set('America/New_York');
 
-
-$dsn = 'mysql:host=localhost;dbname=friendzone';
-$user = 'root';
-$pass = '';
+$dsn = 'mysql:host=us-cdbr-iron-east-01.cleardb.net;dbname=heroku_ba43b6be402db6d';
+$user = 'be662a7b7b2e61';
+$pass = 'c640f8e0';
 
 try {
   $con = new PDO($dsn, $user, $pass);
